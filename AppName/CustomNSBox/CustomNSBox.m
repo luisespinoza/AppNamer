d68b7fb8ea1a921f8cac7e68d66940ee686eda15
Luis Enrique Espinoza Severino <luis.espinoza.severino@gmail.com>
2013-10-10 23:21:23 -0300

    - Trying to make a separator line for boxes :(

--- AppName/CustomNSBox/CustomNSBox.m
+++ AppName/CustomNSBox/CustomNSBox.m
@@ -30,11 +30,26 @@
     [self.fillColor set];
     [bgPath fill];
     
-    //Draw border line
-    [self.borderColor set];
-    [bgPath setLineWidth:1.0];
-    [bgPath stroke];
+    //Draw border lines
+    CGFloat color[4];
+    [self.fillColor getComponents:color];
+    NSColor *lowerColor = [NSColor colorWithSRGBRed:color[0]-60/255. green:color[1]-60/255. blue:color[2]-60/255. alpha:color[3]];
+    [lowerColor set];
+    NSBezierPath *borderLinePath = [NSBezierPath bezierPath];
+    [borderLinePath setLineWidth:1.0];
+    NSPoint p = CGPointMake(dirtyRect.origin.x  , dirtyRect.origin.y+1);
+    NSPoint q = CGPointMake(dirtyRect.origin.x+dirtyRect.size.width, dirtyRect.origin.y+1);
+    [borderLinePath moveToPoint:p];
+    [borderLinePath lineToPoint:q];
+    [borderLinePath stroke];
     
+    NSColor *upperColor = [NSColor colorWithSRGBRed:color[0]+60/255. green:color[1]+60/255. blue:color[2]+60/255. alpha:color[3]];
+    [upperColor set];
+    p = CGPointMake(dirtyRect.origin.x  , dirtyRect.origin.y + dirtyRect.size.height);
+    q = CGPointMake(dirtyRect.origin.x+dirtyRect.size.width, dirtyRect.origin.y + dirtyRect.size.height);
+    [borderLinePath moveToPoint:p];
+    [borderLinePath lineToPoint:q];
+    [borderLinePath stroke];
     //Draw the title
     [[self titleCell] drawWithFrame:[self titleRect] inView:self];
     
